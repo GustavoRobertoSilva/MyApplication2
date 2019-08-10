057e2832c5f205afde416ddedccbda5f4086e9bb
GustavoRobertoSilva <gustaoroberto@gmail.com>
2019-08-09 22:10:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TerceiraActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TerceiraActivity  extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terceira);
+
+
+    }
+
+
+
+
+
+}
